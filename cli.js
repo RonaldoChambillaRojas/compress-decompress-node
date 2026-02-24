1429df932f70900ca281313aadd8f5bc2c1917e0
RonaldoChambillaRojas <ronaldochambillarojas@gmail.com>
2026-02-23 22:13:16 -0500

    feat(app): remove comments

--- cli.js
+++ cli.js
@@ -1,18 +1,13 @@
-// cli.js
 const fs = require('fs');
 const path = require('path');
 const { CLIENT_RENEG_LIMIT } = require('tls');
 
-// Función para leer un archivo de texto en UTF-8
 function readTextFile(filePath) {
   try {
-    // 1️ Leemos el archivo como Buffer (bytes crudos)
     const buffer = fs.readFileSync(filePath);
 
-    // 2️ Lo convertimos a texto UTF-8 para verlo
     const text = buffer.toString('utf-8');
 
-    // 3️ Mostramos información básica
     console.log('--- Información del archivo ---');
     console.log('Ruta:', path.resolve(filePath));
     console.log('Tamaño:', buffer.length, 'bytes');
@@ -20,15 +15,12 @@ function readTextFile(filePath) {
     console.log('\n--- Vista de los bytes ---');
     console.log(buffer);
 
-    // 4️ Devolvemos ambos (texto y buffer) por si los necesitamos luego
     return { buffer, text };
   } catch (err) {
     console.error('Error al leer el archivo:', err.message);
   }
 }
 
-// --- Ejecución simple ---
-// node cli.js input.txt
 const inputFile = process.argv[2];
 
 if (!inputFile) {
